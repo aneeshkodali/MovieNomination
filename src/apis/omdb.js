1dd296ba226e18e9c0999a46eae1d74c232161cd
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-08-26 15:29:36 -0400

    Set up API Request

---
+++ src/apis/omdb.js
@@ -0,0 +1,4 @@
+const KEY = 'd018ff16';
+const omdbUrl = `http://www.omdbapi.com/?apikey=${KEY}&`;
+
+export default omdbUrl;
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Searchbar from './Searchbar';
+import omdbUrl from '../apis/omdb';
+import axios from 'axios';
 
 const App = () => {
 
+
     // function to search for movie term
-    const search = term => {
-        console.log(term);
+    const search = async term => {
+        // make request
+        const response = await axios.get(omdbUrl,{
+            params: {
+                type: 'movie',
+                s: term
+            }
+        });
+
+        // parse out the data
+        const { data } = response;
+
+        console.log(data.Search);
     };
 
-    
+
     return (
         <div>
             <h1>Shoppies App</h1>
