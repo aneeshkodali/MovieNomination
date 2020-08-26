8c347f8182181b500836a6cb4c30687133854f77
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-08-26 14:40:57 -0400

    Add Form to Searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
 
 const Searchbar = () => {
+
+    // state for term
+    const [term, setTerm] = useState('');
+
+    // function to handle search form submission
+    const onSubmit = (event) => {
+        // prevent page from refreshing
+        event.preventDefault();
+        console.log('submitted', term)
+    }
+
     return (
         <div>
-            Searchbar
+            <form onSubmit={onSubmit}>
+                <label>Search for a movie</label>
+                <input
+                    type="text"
+                    value={term}
+                    onChange={(e) => setTerm(e.target.value)}
+                />
+            </form>
         </div>
     );
 };
