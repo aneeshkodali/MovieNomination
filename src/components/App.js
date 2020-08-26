f8f722f2d0b297c7db23f896621ae8f1f682e70c
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-08-26 14:32:01 -0400

    Create Searchbar Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Searchbar from './Searchbar';
 
 const App = () => {
     return (
         <div>
             <h1>Shoppies App</h1>
-            
+            <Searchbar />
         </div>
     );
 };
